e891922d052083194094b9e469c2e44dda66599c
VicKonohaCorporation <vicolachipsofficiel14@Gmail.com>
2024-06-19 20:47:41 +0200

    1.0.1

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "2024 Nouveau Launcher", "author": "Vic" },
+            { "message": "Lancement en cours", "author": "Vic" },
+            { "message": "Gaspard est un hobbit", "author": "Vic" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
